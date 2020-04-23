e572c8fb480e4923d15c57c9910ce2d2ea37ce85
Alexander Serov <asserov@gmail.com>
2020-04-23 17:53:32 +0200

    Add a recommendation based on daily change and not weekly change.

--- main-recommend.py
+++ main-recommend.py
@@ -5,5 +5,5 @@ File allowing to access investment recommendation from the terminal
 from analysis import History
 
 hist_daily = History(interval='day')
-hist_daily.recommend_simple()
-hist_daily.recommend_other()
+hist_daily.recommend_simple(update=True)
+hist_daily.recommend_other(update=False)
