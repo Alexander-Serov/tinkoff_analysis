a1c3f50a002186334b8f041216c0688d33e9fc62
Alexander Serov <asserov@gmail.com>
2021-04-08 22:35:24 +0200

    Add plots, statistics and basic recommendations (#3)
    * Allow to get a simple version of all ETFs' history and plot
    
    * Provide History class that locally stores daily price history and can update online
    
    * Update local table format
    
    * Allow to make a simple recommendation
    
    * Allow simple recommendations 2
    
    * Add colors to simple strategy
    
    * Hotfix price update when the market is open. Should fix #4.
    
    * Allow to log warning to file instead of showing error messages.
    
    * Add a recommendation based on daily change and not weekly change.
    
    * Fix obsolete figis. Output a table with main stats.
    
    * For years, determine the range by a high quantile rather than maximum. Same for minimum. Keep real extrema for weeks. Should increase the statistical robustness of the estimates, while making sure there are no incorrect decisions on the week scale.
    
    * Move and better name logs.
    
    * Update README.md
    
    Co-authored-by: Sergey Serov <sserov@users.noreply.github.com>
    
    * Update requirements.txt
    
    Co-authored-by: Sergey Serov <sserov@users.noreply.github.com>
    
    * Fix nan time bug (#5)
    
    * Never save nan time to the history file.
    
    * Fix logfile name.
    
    * Allow verbose mode for more info.
    Fix bug with wrong timestamps in recently downloaded history.
    
    * Change terminal table print precision.
    
    * Apply changes requested in the review. Complete readme and requirements.
    
    * Clean up and stanardize.
    Update pre-commit config.
    
    Add flake8 checks and standardize.
    
    Add pylint checker.
    
    * Add a test for figi history.
    Fix dataframe creation from dict, cf https://github.com/Alexander-Serov/tinkoff_analysis/pull/3/files#r444338279
    
    * Update .gitignore
    
    Co-authored-by: Sergey Serov <sserov@users.noreply.github.com>
    
    * Sm
    
    * Fix >= bug in statistics.
    
    * Small changes following review.
    
    Co-authored-by: Sergey Serov <sserov@users.noreply.github.com>

---
+++ main-recommend.py
@@ -0,0 +1,9 @@
+"""
+File allowing to access investment recommendation from the terminal
+"""
+
+from analysis import History
+
+hist_daily = History(interval="day", verbose=0)
+hist_daily.recommend_simple(update=True, _print=True)
+hist_daily.recommend_other(update=False)
